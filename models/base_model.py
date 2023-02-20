e78c5087942998934ab1f9226948023ff071c1bf
Ethan1709 <benyayerethan@gmail.com>
2023-02-20 06:18:58 -0800

    update base_model and adding tests for it

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ base class """
+import uuid
+from datetime import datetime
 
 
 class BaseModel:
     """ base class """
-    id = uuid.uuid4()
-    created_at = 
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return (f"[{__class__.__name__}] ({self.id}) {self.__dict__}")
+
+    def save(self):
+        return self.updated_at
+
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = __class__.__name__
+        new_dict["created_at"] = datetime.isoformat(self.created_at)
+        new_dict["updated_at"] = datetime.isoformat(self.updated_at)
+        return new_dict
---
+++ tests/test_base_model.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" test base """
+import unittest
+
+
+
