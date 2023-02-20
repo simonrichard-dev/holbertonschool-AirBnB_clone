57923fbcefa84d1d77453c6200196fada05fb46e
simonrichard-dev <simon.insa@gmail.com>
2023-02-20 16:20:34 +0100

    update test_base_model.py for ID test

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" test base """
+import unittest
+from models.base_model import BaseModel
+from io import StringIO
+
+
+class TestBase(unittest.TestCase):
+    """ unittest """
+    def test_init(self):
+        basemodel = BaseModel()
+        basemodel.id = "3th4N - 5im0n"
+        self.assertEqual(basemodel.id, "3th4N - 5im0n")
