6816ca6007ee71bb5cc0a7b83e137805557caa97
Ethan1709 <benyayerethan@gmail.com>
2023-02-21 06:53:40 -0800

    updating base and file_storage

--- models/base_model.py
+++ models/base_model.py
@@ -10,9 +10,19 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                setattr(self, key, value)
+                if key == "id":
+                    self.id = value
+                if key == "created_at":
+                    self.created_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "updated_at":
+                    self.updated_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.created_at = datetime.now()
+                if key == "updated_at":
+                    self.updated_at = datetime.now()
+                if key == "my_number":
+                    self.my_number = value
+                if key == "name":
+                    self.name = value
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
