6c19fd2e6c40dfa7f1a1e460ab2b5c9d6a73b77e
simonrichard-dev <simon.insa@gmail.com>
2023-02-21 17:49:36 +0100

    update do_create and do show features in console.py file

--- console.py
+++ console.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
 """ base class """
-import cmd, sys
+import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """class HBNB for Command Interpreter
     """
     prompt = '(hbnb) '
+    list_class = ['BaseModel']
 
-    def do_EOF(self, line):
+    list_function = ['create']
+
+    def precmd(self, arg):
+        """parses command input"""
+        if '.' in arg and '(' in arg and ')' in arg:
+            my_class = arg.split('.')
+            my_func = my_class[1].split('(')
+            param=my_func[1].split(')')
+            if my_class[0] in HBNBCommand.list_class and \
+               my_func[0] in HBNBCommand.list_function:
+                arg = my_func[0] + ' ' + my_class[0] + ' ' + param[0]
+        return arg
+
+    def do_EOF(self, arg):
         """execute End of File line
         """
         return True
     
-    def do_quit(self, line):
+    def do_quit(self, arg):
         """Quit commant to exit the program
         """
         return True
@@ -22,6 +38,27 @@ class HBNBCommand(cmd.Cmd):
         """Do not repeat previous action"""
         pass
 
+    def do_create(self, arg):
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.list_class:
+            print("** class doesn't exist **")
+        else:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+    
+    def do_show(self, arg):
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.list_class:
+            print("** class doesn't exist **")
+        elif arg not in id:
+            print('** instance id missing **')
+        elif new_instance not in id:
+            print('** no instance found **')
+        else:
+            print(new_instance.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
