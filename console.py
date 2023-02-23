e734ef32188f10e083047a66f4a87420db9a88b7
Ethan1709 <benyayerethan@gmail.com>
2023-02-23 01:29:41 -0800

    removing useless function

--- console.py
+++ console.py
@@ -19,18 +19,6 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(shotgun !!!) '
     list_class = ['BaseModel', 'User', 'State', 'City', 'Amenity', 'Place', 'Review']
 
-    list_function = ['create']
-
-    # def precmd(self, arg):
-    #    """parses command input"""
-    #    if '.' in arg and '(' in arg and ')' in arg:
-    #        my_class = arg.split('.')
-    #        my_func = my_class[1].split('(')
-    #        param=my_func[1].split(')')
-    #        if my_class[0] in HBNBCommand.list_class and \
-    #           my_func[0] in HBNBCommand.list_function:
-    #            arg = my_func[0] + ' ' + my_class[0] + ' ' + param[0]
-    #    return arg
 
     def do_EOF(self, arg):
         """execute End of File line
