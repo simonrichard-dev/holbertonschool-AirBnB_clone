0772465eb4cad66aa061b3c16a5c193b1cc00c8b
Ethan1709 <benyayerethan@gmail.com>
2023-02-23 05:21:50 -0800

    updating test_user file

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -3,7 +3,7 @@
 import unittest
 from models.base_model import BaseModel
 from models.user import User
-
+import datetime
 from io import StringIO
 
 
@@ -29,3 +29,33 @@ class TestUser(unittest.TestCase):
         user = User()
         user.last_name = "Barbier"
         self.assertEqual(user.last_name, "Barbier")
+
+    user = User()
+
+    def testHasAttributes(self):
+        """verify if attributes exist"""
+        self.assertTrue(hasattr(self.user, 'email'))
+        self.assertTrue(hasattr(self.user, 'password'))
+        self.assertTrue(hasattr(self.user, 'first_name'))
+        self.assertTrue(hasattr(self.user, 'last_name'))
+        self.assertTrue(hasattr(self.user, 'id'))
+        self.assertTrue(hasattr(self.user, 'created_at'))
+        self.assertTrue(hasattr(self.user, 'updated_at'))
+
+    def test_types(self):
+        """tests if the type of the attribute is the correct one"""
+        self.assertIsInstance(self.user.email, str)
+        self.assertIsInstance(self.user.password, str)
+        self.assertIsInstance(self.user.first_name, str)
+        self.assertIsInstance(self.user.last_name, str)
+        self.assertIsInstance(self.user.id, str)
+        self.assertIsInstance(self.user.created_at, datetime.datetime)
+        self.assertIsInstance(self.user.updated_at, datetime.datetime)
+
+    def test_user_inheritance(self):
+        """test if User is a subclass of BaseModel"""
+        self.assertIsInstance(self.user, User)
+
+    def test_class_exist(self):
+        self.assertEqual(str(type(self.user)), "<class 'models.user.User'>")
+
