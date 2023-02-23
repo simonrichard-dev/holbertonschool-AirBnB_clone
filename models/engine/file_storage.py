9cdcce2cbad0bd25999826c3f5cb5aa4980cf6b5
Ethan1709 <benyayerethan@gmail.com>
2023-02-23 08:08:31 -0800

    fixing file_storage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,7 +16,7 @@ class FileStorage:
     __objects = {}
 
     def all(self):
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         key = f"{obj.__class__.__name__}.{obj.id}"
